2acf1f1099d258b44684f70d8b875999e5e1cdda
tanqing <quincytann@qq.com>
2021-03-21 19:31:53 +0800

    使用toolbar代替actionbar,给唤出选项菜单预留入口

--- app/src/main/java/com/example/planegeometry/MainActivity.kt
+++ app/src/main/java/com/example/planegeometry/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.planegeometry
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.draw_layout.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,50 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.draw_layout)
-//        val sketchpad = findViewById<Sketchpad>(R.id.myView)
-//        /**
-//         * 红色
-//         */
-//        findViewById<Button>(R.id.btn1).setOnClickListener(View.OnClickListener {
-//            sketchpad.setPaintColor(
-//                    Color.RED
-//            )
-//        })
-//        /**
-//         * 蓝色
-//         */
-//        findViewById<Button>(R.id.btn2).setOnClickListener(View.OnClickListener {
-//            sketchpad.setPaintColor(
-//                    Color.BLUE
-//            )
-//        })
-//        /**
-//         * 矩形
-//         */
-//        findViewById<Button>(R.id.btn3).setOnClickListener(View.OnClickListener {
-//            sketchpad.setPaintMode(
-//                    Sketchpad.RECT
-//            )
-//        })
-//        /**
-//         * 圆形
-//         */
-//        findViewById<Button>(R.id.btn4).setOnClickListener(View.OnClickListener {
-//            sketchpad.setPaintMode(
-//                    Sketchpad.CIRCLE
-//            )
-//        })
-//        /**
-//         * 画笔
-//         */
-//        findViewById<Button>(R.id.btn5).setOnClickListener(View.OnClickListener {
-//            sketchpad.setPaintMode(
-//                    Sketchpad.PEN
-//            )
-//        })
-//        /**
-//         * 清除
-//         */
-//        findViewById<Button>(R.id.btn6).setOnClickListener(View.OnClickListener { sketchpad.clear() })
+        setSupportActionBar(toolbar)
+
     }
 }
